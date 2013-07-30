78e3ac04ed65bc5bbf45b449ae84599d0eb07443
jlazear <jlazear@gmail.com>
2013-07-30 14:49:50 -0400

    Added setup.py. May now do python setup.py install.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Cereal',
+      version='130620.dev',
+      description='Threaded buffered drop-in replacement for PySerial.Serial',
+      author='Justin Lazear',
+      author_email='jlazear@gmail.com',
+      url='http://www.github.com/jlazear/cereal',
+      py_modules=['cereal']
+      )
